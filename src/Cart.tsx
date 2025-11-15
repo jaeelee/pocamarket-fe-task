15190ef98c639f9192c697c5d1ec535bd692a276
jaeelee <jaeelee.dev@gmail.com>
2025-11-16 01:38:05 +0900

    feat: 전체선택 & 삭제 기능 추가

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -1,6 +1,7 @@
 import { HomeIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Header } from "./_components/Header";
+import { useShoppingList } from "./_libs/shopping.store";
 
 export function Cart() {
   /**
@@ -9,16 +10,10 @@ export function Cart() {
    * 2. 상단 헤더 완성
    * 3. 결제 금액 영역 완성
    *
-   */
-  const checkItemCount = 0;
-  const totalItemCount = 4;
+  */
+
   const userCash = 100;
-  const exampleCartItems = [
-    { id: 1, name: "Item 1", price: 10 },
-    { id: 2, name: "Item 2", price: 20 },
-    { id: 3, name: "Item 3", price: 30 },
-    { id: 4, name: "Item 4", price: 40 },
-  ];
+  const { list: cartList, selecteAll, selected, selecteList, removeList, removeSelectedList } = useShoppingList();
 
   return (
     <div className="relative">
@@ -32,29 +27,34 @@ export function Cart() {
       <div>
         <div className="h-15 flex gap-6 items-center border-b border-gray-100">
           <div className="flex gap-2 items-center px-4">
-            <input type="checkbox" />
-            <button className="text-sm text-gray-500 hover:text-blue-500">
-              전체 선택 ({checkItemCount}/{totalItemCount})
+            <input type="checkbox" checked={cartList.length === selected.length} onClick={selecteAll} />
+            <button className="text-sm text-gray-500 hover:text-blue-500" >
+              전체 선택 ({selected.length}/{cartList.length})
             </button>
           </div>
-          <button className="bg-gray-200 text-sm text-gray-500 hover:text-blue-500 p-2 rounded-md cursor-pointer">
+          <button className="bg-gray-200 text-sm text-gray-500 hover:text-blue-500 p-2 rounded-md cursor-pointer" onClick={() => removeSelectedList()}>
             선택 제거
           </button>
         </div>
         <div className="flex flex-col md:flex-row">
           <section className="flex flex-1 flex-col p-4">
-            {exampleCartItems.map((item) => (
+            {cartList.map((item) => (
               <div
                 key={item.id}
                 className="flex items-center justify-between p-2 border-b border-gray-100"
               >
                 <div className="flex items-center">
-                  <input type="checkbox" className="mr-2" />
+                  <input
+                    type="checkbox"
+                    className="mr-2"
+                    checked={selected.includes(item.id)}
+                    onClick={() => selecteList(item.id)}
+                  />
                   <span>
                     {item.name} - USD {item.price}
                   </span>
                 </div>
-                <button className="text-red-500 hover:text-red-700">
+                <button className="text-red-500 hover:text-red-700" onClick={() => removeList(item.id)}>
                   삭제
                 </button>
               </div>
@@ -65,7 +65,7 @@ export function Cart() {
             <div className="w-full flex flex-col gap-1">
               <p className="flex justify-between">
                 전체 아이템 수:{" "}
-                <span className="font-bold">{totalItemCount}개</span>
+                <span className="font-bold">{cartList.length}개</span>
               </p>
               <p className="flex justify-between">
                 전체 상품 금액: <span className="font-bold">USD {0}</span>
--- src/_libs/shopping.store.ts
+++ src/_libs/shopping.store.ts
@@ -5,8 +5,10 @@ interface ShoppingListStore {
     list: Card[],
     selected: number[],
     addList: (item: Card) => void;
-    selecteList: (id: number) => void;
     removeList: (id: number) => void;
+    selecteAll: () => void;
+    selecteList: (id: number) => void;
+    removeSelectedList: () => void;
 }
 
 
@@ -14,18 +16,29 @@ export const useShoppingList = create<ShoppingListStore>((set, get) => ({
     list: [],
     selected: [],
     addList: (item) => set((state) => {
-        if (state.list.length === 20) return state;
-        if (state.list.find(el => el.id === item.id)) return state;
+        if (state.list.length === 20) { alert('담은 물건이 20개 이상입니다.'); return state; }
+        if (state.list.find(el => el.id === item.id)) { alert('이미 추가된 카드 입니다.'); return state; }
 
+        alert('장바구니 추가 완료')
         return { list: [item, ...state.list] }
     }),
-    selecteList: (id) => set((state) => ({
-        selected: [id, ...state.selected]
-    })),
+    removeList: (id) => set((state) => ({ list: state.list.filter(item => item.id !== id) })),
+    selecteAll: () => set((state) => {
+        if (state.selected.length === state.list.length) return { selected: [] }
+        return { selected: [...state.list.map(item => item.id)] }
+    }),
+    selecteList: (id) => set((state) => {
+        if (state.selected.includes(id)) return { selected: state.selected.filter(selectedId => selectedId !== id) }
+        return {
+            selected: [id, ...state.selected]
+        }
+    }),
     selectedList: () => {
         const { list, selected } = get();
         return list.find(el => selected.includes(el.id)) || null;
     },
-    removeList: (id) => set((state) => ({ list: state.list.filter(item => item.id !== id) }))
-
+    removeSelectedList: () => {
+        const { selected, removeList } = get();
+        selected.map((id) => removeList(id))
+    }
 }))
