17f67a6cd16b1e9ac3efc2525f8cc1198c0b13fd
jaeelee <jaeelee.dev@gmail.com>
2025-11-15 23:18:37 +0900

    feat: 모달 추가

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,11 @@
 import { Header } from "./_components/Header";
-import { ShoppingCartIcon } from "lucide-react";
+import { ShoppingCartIcon, HeartIcon, ThumbsUpIcon, ThumbsUp } from "lucide-react";
 import { Link } from "react-router-dom";
 import { TYPE_DATA } from "./_libs/type.data";
 import { getPaginatedCard } from "./_libs/getPaginatedCard";
 import { Card } from "./_libs/card.type";
 import { useEffect, useState } from "react";
+import Modal from "./Modal";
 
 export function Home() {
   /**
@@ -14,6 +15,8 @@ export function Home() {
   const [totalPages, setTotalPages] = useState<number>(0);
   const [cards, setCards] = useState<Card[]>([]);
   const [selectedGroup, setSelectedGroup] = useState<string | undefined>(undefined);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState<Card>();
 
   useEffect(() => {
     const fetchCards = async () => {
@@ -39,52 +42,84 @@ export function Home() {
   };
 
   return (
-    <div>
-      <Header
-        linkComponent={
-          <Link to="/cart">
-            <ShoppingCartIcon />
-          </Link>
-        }
-      />
-      <div className="flex">
-        <nav className="w-1/4 bg-gray-100 pt-4 sticky h-[calc(100vh-60px)] top-15">
-          <div className="flex flex-col gap-2">
-            {TYPE_DATA.map((type) => (
-              <button
-                key={`type-${type}`}
-                className={`text-sm hover:text-blue-500 w-full ${selectedGroup === type ? 'text-blue-500' : 'text-gray-500'} `}
-                onClick={() => setSelectedGroup(prev => prev === type ? undefined : type)}
-              >
-                {type}
-              </button>
-            ))}
-          </div>
-        </nav>
-        <section className="flex-1 px-4 py-4">
-          <div className="grid grid-cols-4 gap-4">
-            {cards.filter(item => selectedGroup ? item.groupName.includes(selectedGroup) : item).map((item, index) => (
-              <div key={`${index}`}>
-                <h2 className="text-lg font-semibold">{item.name} ({item.memberName})</h2>
-                <p className="text-sm text-gray-500">{item.groupName}</p>
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className="w-full h-auto rounded-md"
-                />
-                <div className="flex flex-row gap-1 items-">
-                  <p className="text-lg font-semibold text-red-400">{Math.round(item.discountRate * 100)}%</p>
-                  <p className="text-lg font-semibold text-gray-500">{item.discountedPrice} USD</p>
-                  <p className="text-sm font-semibold text-gray-500 line-through">{item.price} USD</p>
+    <>
+      <div>
+        <Header
+          linkComponent={
+            <Link to="/cart">
+              <ShoppingCartIcon />
+            </Link>
+          }
+        />
+        <div className="flex">
+          <nav className="w-1/4 bg-gray-100 pt-4 sticky h-[calc(100vh-60px)] top-15">
+            <div className="flex flex-col gap-2">
+              {TYPE_DATA.map((type) => (
+                <button
+                  key={`type-${type}`}
+                  className={`text-sm hover:text-blue-500 w-full ${selectedGroup === type ? 'text-blue-500' : 'text-gray-500'} `}
+                  onClick={() => setSelectedGroup(prev => prev === type ? undefined : type)}
+                >
+                  {type}
+                </button>
+              ))}
+            </div>
+          </nav>
+          <section className="flex-1 px-4 py-4">
+            <div className="grid grid-cols-4 gap-4">
+              {cards.filter(item => selectedGroup ? item.groupName.includes(selectedGroup) : item).map((item, index) => (
+                <div key={`${index}`} onClick={() => {
+                  setSelectedCard(item);
+                  setIsOpen(true)
+                }}>
+                  <h2 className="text-lg font-semibold">{item.name} ({item.memberName})</h2>
+                  <p className="text-sm text-gray-500">{item.groupName}</p>
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="w-full h-auto rounded-md"
+                  />
+                  <div className="flex gap-1">
+                    <p className="text-lg font-semibold text-red-400">{Math.round(item.discountRate * 100)}%</p>
+                    <p className="text-lg font-semibold text-gray-500">{item.discountedPrice} USD</p>
+                    <p className="text-sm font-semibold text-gray-500 line-through">{item.price} USD</p>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </section>
+              ))}
+            </div>
+          </section>
+        </div>
+        <span className="text-sm text-gray-500 bg-white p-3 rounded shadow-lg fixed bottom-4 right-4">
+          ({currentPage ?? 0}/{totalPages ?? 0})
+        </span>
       </div>
-      <span className="text-sm text-gray-500 bg-white p-3 rounded shadow-lg fixed bottom-4 right-4">
-        ({currentPage ?? 0}/{totalPages ?? 0})
-      </span>
-    </div>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <div className="p-5 bg-white rounded-md">
+          <h2 className="text-2xl font-semibold">{selectedCard?.name} ({selectedCard?.memberName})</h2>
+          <p className="text-base text-gray-500">{selectedCard?.groupName}</p>
+          <div>
+            <img
+              src={selectedCard?.image}
+              alt={selectedCard?.name}
+              className="w-full h-auto rounded-md"
+            />
+          </div>
+          <div className="flex justify-between">
+            <div className="flex gap-1">
+              <p className="text-lg font-semibold text-red-400">{Math.round((selectedCard?.discountRate ?? 0) * 100)}%</p>
+              <p className="text-lg font-semibold text-gray-500">{selectedCard?.discountedPrice} USD</p>
+              <p className="text-sm font-semibold text-gray-500 line-through">{selectedCard?.price} USD</p>
+            </div>
+            <div>
+              <div className="flex gap-5 justify-end">
+                <div className="flex gap-2"><ThumbsUpIcon fill={selectedCard?.bestPick ? 'pink' : 'white'} />best</div>
+                <div className="flex gap-2"><HeartIcon />{selectedCard?.wishCount}</div>
+              </div>
+              <div className="flex gap-2 justify-end mt-3"><ShoppingCartIcon />장바구니 담기</div>
+            </div>
+          </div>
+        </div>
+      </Modal >
+    </>
   );
 }
